62cadf7e84576ed793389b6440bacb6ca087994a
Anathomical <anathomical@anathomical.com>
2012-04-08 16:39:58 +0000

    Do some cleanup work on the views and layout so that it looks cleaner and uses some correct Bootstrap syntax.

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,8 +1,9 @@
 $(function(){
-	$('form.user_roles').on('submit', function(evt) {
+	$('form.ajax-intercept-form').on('submit', function(evt) {
 		evt.preventDefault();
-		var data = {}
-		var inputs = $(evt.target).find('input');
+		var form = $(evt.target);
+		var data = {};
+		var inputs = form.find('input');
                 for(var i = 0; i < inputs.length; i++){
 			if(inputs[i].name){
 				if(inputs[i].type == "checkbox")
@@ -11,9 +12,13 @@ $(function(){
 					data[inputs[i].name] = inputs[i].value;
 			}
 		}
-		$.post('/admin/update_user_roles', data, function(ret){
-			console.log('post complete');
-			console.log(ret);
+		$.ajax({
+			'type': form.attr('method'),
+			'url': form.attr('action'),
+			'data':  data,
+			'success': function(ret){
+				console.log(ret);
+			}
 		});
 		return false;
 	});
