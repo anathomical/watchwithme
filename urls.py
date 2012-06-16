c88df48208a90443c5ef2d8b1df13908b004ae7b
Anathomical <anathomical@anathomical.com>
2012-06-16 01:11:59 +0000

    Clean up the urls list, primarily by marking url ends with /?$ to allow for trailing slashes.

--- urls.py
+++ urls.py
@@ -1,18 +1,16 @@
 import controllers
 import config
-from tornado.web import StaticFileHandler
 
 URLS = [
-    (r"^/robots.txt$", StaticFileHandler, dict(path=config.APPLICATION['static_path'])),
     (r"^/$", controllers.main),
-    (r"^/join/?([\w\d]*)", controllers.join),
-    (r"^/login", controllers.login),
-    (r"^/logout", controllers.logout),
-    (r"^/profile", controllers.user_profile),
-    (r"^/admin$", controllers.admin_panel),
-    (r"^/admin/update_user_roles$", controllers.change_roles),
-    (r"^/upload$", controllers.upload),
-    (r"^/room/([\w\d]+)/socket", controllers.room_socket),
-    (r"^/room/([\w\d]+)", controllers.room)
+    (r"^/join/?([\w\d]*)/?$", controllers.join),
+    (r"^/login/?$", controllers.login),
+    (r"^/logout/?$", controllers.logout),
+    (r"^/profile/?$", controllers.user_profile),
+    (r"^/admin/?$", controllers.admin_panel),
+    (r"^/admin/update_user_roles/?$", controllers.change_roles),
+    (r"^/upload/?$", controllers.upload),
+    (r"^/room/([\w\d]+)/socket/?$", controllers.room_socket),
+    (r"^/room/([\w\d]+)/?$", controllers.room),
 ]
     
